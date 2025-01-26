696e7e7eec31c6ac42ba19ab71c073486e585989
Mohamed Wasim <wasimmdmohamed@gmail.com>
2025-01-26 17:58:48 +0530

    chore: switch from JSON logger to aiologger default handlers for async logging to console

--- blck.py
+++ blck.py
@@ -2,7 +2,7 @@ import os
 import time
 import asyncio
 import magic
-from aiologger.loggers.json import JsonLogger
+from aiologger import Logger
 from aiologger.handlers.files import AsyncFileHandler
 from sanic import Sanic, Blueprint
 from sanic.response import json, html, file
@@ -20,14 +20,16 @@ args = parser.parse_args()
 project_dir = os.getcwd()
 log_file_path = os.path.join(project_dir, 'blck.log')
 
-logger = JsonLogger.with_default_handlers(level='DEBUG' if args.debug else 'INFO', flatten=True)
-handler = AsyncFileHandler(log_file_path)
-logger.add_handler(handler)
+logger = Logger.with_default_handlers(level='DEBUG' if args.debug else 'INFO')
+
+file_handler = AsyncFileHandler(log_file_path)
+logger.add_handler(file_handler)
+
 
 app = Sanic(__name__)
 bp = Blueprint('blck', url_prefix='/')
 files_dir = 'files'
-os.makedirs(files_dir, exist_ok=True)
+os.makedirs(files_dir, exist_ok=True)	
 
 bp.static('/files', files_dir)
 jinja_env = Environment(loader=FileSystemLoader('templates'))
@@ -42,13 +44,13 @@ def get_current_time():
 @bp.route("/health")
 async def health_check(request):
     current_time = get_current_time()
-    await logger.debug(f"Health check route accessed at {current_time} with the method: {request.method}")
+    await logger.debug(f"{current_time}: Health check route accessed with the method: {request.method}")
     return json({'status': 'healthy'})
 
 @bp.route("/", methods=['GET', 'POST'])
 async def index(request):
     current_time = get_current_time()
-    await logger.debug(f"Index route accessed at {current_time} with the method: {request.method}")
+    await logger.debug(f"{current_time}: Index route accessed with the method: {request.method}")
     scheme = request.headers.get('X-Forwarded-Proto', request.scheme)
     base_url = f"{scheme}://{request.host}/"
     if request.method == 'GET':
@@ -58,7 +60,7 @@ async def index(request):
 @bp.route("/<urlshort>")
 async def urlget(request, urlshort):
     current_time = get_current_time()
-    await logger.debug(f"Request received for URL short: {urlshort} at {current_time}")
+    await logger.debug(f"{current_time}: Request received for URL short: {urlshort}")
     file_path = next((os.path.join(files_dir, f) for f in os.listdir(files_dir) if f.startswith(urlshort)), None)
     if not file_path or not os.path.isfile(file_path):
         return json({'error': 'File not found'})
@@ -70,18 +72,18 @@ async def urlget(request, urlshort):
         os.remove(file_path)
         return response
     except Exception as e:
-        await logger.error(f"Error serving file: {e} at {current_time}")
+        await logger.error(f"{current_time}: Error serving file: {e}")
         return json({'error': 'Error serving file'})
 
 async def handle_file_upload(request, base_url):
     current_time = get_current_time()
-    await logger.debug(f"Handling file upload at {current_time} with the method: {request.method}")
+    await logger.debug(f"{current_time}: Handling file upload with the method: {request.method}")
     if not request.files or 'c' not in request.files:
         return json({'error': 'No file provided'})
 
     file_urls = []
     for file in request.files['c']:
-        await logger.debug(f"Processing file: {file.name} with size: {len(file.body)} bytes at {current_time}")
+        await logger.debug(f"{current_time}: Processing file: {file.name} with size: {len(file.body)} bytes")
         file_id = ''.join(choice(ascii_uppercase + ascii_lowercase) for _ in range(6))
         file_path = os.path.join(files_dir, file_id)
 
@@ -91,7 +93,7 @@ async def handle_file_upload(request, base_url):
         mime_type = file.type or magic.from_file(file_path, mime=True)
         if mime_type:
             os.rename(file_path, f"{file_path}.{mime_type.split('/')[1]}")
-            await logger.debug(f"File renamed to: {file_path}.{mime_type.split('/')[1]} at {current_time}")
+            await logger.debug(f"{current_time}: File renamed to: {file_path}.{mime_type.split('/')[1]}")
 
         file_metadata[file_id] = {'last_accessed': time.time()}
         file_url = f"{base_url}{file_id}"
@@ -102,14 +104,14 @@ async def handle_file_upload(request, base_url):
 async def clean_up_expired_files():
     while True:
         current_time = get_current_time()
-        await logger.debug(f"Running cleanup tasks for expired files at {current_time}.")
+        await logger.debug(f"{current_time}: Running cleanup tasks for expired files")
         current_time = time.time()
         for file_id, metadata in list(file_metadata.items()):
             last_accessed = metadata['last_accessed']
             if current_time - last_accessed > EXPIRATION_TIME:
                 file_path = os.path.join(files_dir, file_id)
                 if os.path.exists(file_path):
-                    await logger.debug(f"Expired file {file_id} deleted: {file_path} at {current_time}")
+                    await logger.debug(f"{current_time}: Expired file {file_id} deleted: {file_path}")
                     os.remove(file_path)
                 del file_metadata[file_id]
 
@@ -118,9 +120,9 @@ async def clean_up_expired_files():
 @bp.listener('before_server_start')
 async def before_server_start(app, loop):
     current_time = get_current_time()
-    await logger.debug(f"Adding cleanup task at {current_time}.")
+    await logger.debug(f"{current_time}: Adding cleanup task")
     app.add_task(clean_up_expired_files())
-    await logger.debug(f"Starting sanic application at {current_time}")
+    await logger.debug(f"{current_time}: Starting sanic application")
 
 app.blueprint(bp)
 
